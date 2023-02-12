89a2be77043a4164d9c33910e1d8d292d81fad08
František Kasík <frantis3k.kasik@gmail.com>
2023-02-12 18:29:38 +0100

    Add function to valide players input and function to play a round

--- script.js
+++ script.js
@@ -2,23 +2,81 @@
 
 function getComputerChoice() {
     let choiceInt = Math.floor(Math.random() * 3);
-    let choiceString = "";
-
-    if (choiceInt === 0) {
-        choiceString = "rock";
-    } else if (choiceInt === 1) {
-        choiceString = "paper";
-    } else {
-        choiceString = "scissors";
-    }
     
-    return choiceString;
+    return choiceInt;
+}
+
+function validateChoice(playerChoice) {
+    playerChoice = choiceToNumber(playerChoice);
+
+    while (playerChoice === -1) {
+        playerChoice = choiceToNumber(prompt("Incorrect choice, try paper, rock or scissors.", "Rock"));
+    }
+
+    return playerChoice;
+}
+
+function choiceToNumber(playerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+
+    if (playerChoice === "rock") {
+        playerChoice = 0;
+    } else if (playerChoice === "paper") {
+        playerChoice = 1;
+    } else if (playerChoice === "scissors") {
+        playerChoice = 2;
+    } else {
+        playerChoice = -1;
+    }
+
+    return playerChoice;
 }
 
 function getPlayerChoice() {
     let playerChoice = prompt("Rock, paper or scissors?", "Rock");
-
-    return playerChoice.toLowerCase();
+    playerChoice = validateChoice(playerChoice);
+    
+    return playerChoice;
 }
 
-console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection) {
+    let roundWinner = "";
+
+    if (playerSelection === computerSelection) {
+        roundWinner = "draw";
+    } else {
+        if (playerSelection === 0 && computerSelection === 2) {
+            roundWinner = "player";
+        } else {
+            roundWinner = "computer";
+        }
+
+        if (playerSelection === 1 && computerSelection === 0) {
+            roundWinner = "player";
+        } else {
+            roundWinner = "computer";
+        }
+
+        if (playerSelection === 2 && computerSelection === 1) {
+            roundWinner = "player";
+        } else {
+            roundWinner = "computer";
+        }
+    }
+
+    return roundWinner;
+
+}
+
+function game(rounds = 5) {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= rounds; i++) {
+
+    }
+}
+
+console.log(getPlayerChoice());
