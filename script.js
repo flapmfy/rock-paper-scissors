ed8ddc58591bd3fb89d67ec31cf54b81c09ea998
František Kasík <frantis3k.kasik@gmail.com>
2023-02-17 20:18:51 +0100

    Change everything

--- script.js
+++ script.js
@@ -1,73 +1,119 @@
 "use strict";
 
-function getComputerChoice() {
-    let choiceInt = Math.floor(Math.random() * 3);
-    let choiceString = "";
-    
-    if (choiceInt === 0) {
-        choiceString = "rock";
-    } else if (choiceInt === 1) {
-        choiceString = "paper";
-    } else if (choiceInt === 2) {
-        choiceString = "scissors";
-    }
+let playerScore = 0;
+let computerScore = 0;
+const playerScoreOut = document.querySelector(".js-player-score");
+const computerScoreOut = document.querySelector(".js-computer-score");
+const roundWinnerOut = document.querySelector(".js-round-winner");
+const gameWinnerOut = document.querySelector(".js-winner");
+const resetGameButton = document.querySelector(".js-reset");
+const playerEmoji = document.querySelector(".player-emoji");
+const computerEmoji = document.querySelector(".computer-emoji");
 
-    return choiceString;
-}
+resetGameButton.addEventListener('click', resetGame);
 
 const buttons = document.querySelectorAll(".choice");
-buttons.forEach(button => button.addEventListener('click', getPlayerChoice));
+buttons.forEach((button) => button.addEventListener("click", playRound));
 
-function getPlayerChoice(e) {
-    let playerChoice = e.target;
-    console.log(playerChoice);
+function getComputerChoice() {
+  let choiceInt = Math.floor(Math.random() * 3);
+  let choiceString = "";
 
-    return playerChoice;
+  if (choiceInt === 0) {
+    choiceString = "rock";
+  } else if (choiceInt === 1) {
+    choiceString = "paper";
+  } else if (choiceInt === 2) {
+    choiceString = "scissors";
+  }
+
+  return choiceString;
 }
 
-function playRound(playerSelection, computerSelection) {
+function getPlayerChoice(e) {
+  let playerChoice = e.currentTarget.lastElementChild.textContent;
+  return playerChoice.toLowerCase();
+}
+
+function playRound(e) {
+  let roundWinner = "";
+  let winner = "";
+  let playerSelection = getPlayerChoice(e);
+  let computerSelection = getComputerChoice();
+
+  drawChoice(playerSelection, playerEmoji);
+  drawChoice(computerSelection, computerEmoji);
+
+  if (playerScore !== 5 && computerScore !== 5) {
+    roundWinner = checkRoundWinner(playerSelection, computerSelection);
+    roundWinnerOut.textContent = roundWinner;
+  }
+
+    if (playerScore === 5 || computerScore === 5) {
+    winner = getGameWinner();
+    gameWinnerOut.textContent = winner;
+    resetGameButton.style.display = "block";
+  }
+}
+
+
+function checkRoundWinner (playerSelection, computerSelection) {
     let roundWinner = "";
 
     if (playerSelection === computerSelection) {
         roundWinner = "draw";
-    } else {
+      } else {
         if (playerSelection === "rock" && computerSelection === "scissors") {
-            roundWinner = "player";
+          roundWinner = "player";
         } else if (playerSelection === "paper" && computerSelection === "rock") {
-            roundWinner = "player";
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            roundWinner = "player";
+          roundWinner = "player";
+        } else if (
+          playerSelection === "scissors" &&
+          computerSelection === "paper"
+        ) {
+          roundWinner = "player";
         } else {
-            roundWinner = "computer";
+          roundWinner = "computer";
         }
-    }
+      }
+      updateScore(roundWinner);
 
-    return roundWinner;
+      return roundWinner;
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+function updateScore(roundWinner) {
+    if (roundWinner === "player") playerScore++;
+    else if (roundWinner === "computer") computerScore++;
 
-    for (let i = 1; i <= 5; i++) {
-
-
-        let roundWinner = "";
-        do {
-            let playerChoice = getPlayerChoice();
-            let computerChoice = getComputerChoice();
-            roundWinner = playRound(playerChoice, computerChoice);
-        } while (roundWinner === "draw")
-
-        if (roundWinner === "player") {
-            playerScore++;
-        } else {
-            computerScore++;
-        }
-    }
+    playerScoreOut.textContent = playerScore;
+    computerScoreOut.textContent = computerScore;
 }
 
+function getGameWinner() {
+    if (playerScore === 5) return "player";
+    return "computer";
+}
 
-////////////////////////////tests
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreOut.textContent = 0;
+    computerScoreOut.textContent = 0;
+    roundWinnerOut.textContent = "";
+    gameWinnerOut.textContent = "";
+    resetGameButton.style.display = "none";
+    drawChoice("no", playerEmoji);
+    drawChoice("no", computerEmoji);
+}
 
-game(5);
+function drawChoice(choice, where) {
+    if (choice === "rock") {
+        where.textContent = "🪨";
+    } else if (choice === "paper") {
+        where.textContent = "🤚";
+    } else if (choice === "scissors") {
+        where.textContent = "✂️";
+    } else {
+        where.textContent = "🤔";
+    }
+}
